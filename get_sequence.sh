2368bf98beac860062d06323998f15797ed1c3c3
Giulio Vidotto <134842955+GiulioVidotto@users.noreply.github.com>
2025-01-11 13:04:18 +0100

    Add files via upload

---
+++ get_sequence.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Array with all the chromosome numbers
+chr_num_array=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "X" "Y")
+
+# Chromosome M is not present becaus no UTR regions were identify on that chromosome
+
+# For loop that iterates through all the sample numbers
+for chr_num in "${chr_num_array[@]}"; do
+    
+    # Define the bed_file name
+    bed_file="$HOME/Validation_step/bed_and_fasta_file/utr_with_poly_A_bed_file/3UTR_poly_A_regions_chr${chr_num}.bed"
+    
+    # Define the name of the fasta file of the chromosome
+    chr_fasta_file="$HOME/Validation_step/bed_and_fasta_file/chr_fasta_file/chr${chr_num}.fa"
+    
+    # Define the sequence_output name
+    sequence_output="$HOME/Validation_step/bed_and_fasta_file/utr_with_poly_A_output/3UTR_with_poly_A_sequences_DNA/3UTR_with_poly_A_sequences_on_chr${chr_num}.fa"
+    
+    # Get the sequences
+    bedtools getfasta -name -s -fi ${chr_fasta_file} -bed ${bed_file} -fo ${sequence_output}
+  
+done
