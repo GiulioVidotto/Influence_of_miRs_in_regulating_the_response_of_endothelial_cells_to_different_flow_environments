56bd6308b6b32d4274e295ae45f72c87aabbee6d
Giulio Vidotto <134842955+GiulioVidotto@users.noreply.github.com>
2025-01-13 12:15:11 +0100

    Update and rename download_public_data to download_public_data.sh

--- download_public_data.sh
+++ download_public_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Define project data directory using the HOME environment variable
-project_data="$HOME/project_data"
+# Define project data directory
+project_data="$(pwd)/project_data"
 
 # __ miRBase file __
 # Decide the folder where the data from miRBase (miRNA notiation) will be stored
 miRBase_download_folder="$project_data/miR_database/miRBase_database"
 # Download the file stored in miRBase
-download_file.sh "https://www.mirbase.org/download/mature.fa" "$miRBase_download_folder"
+./download_file.sh "https://www.mirbase.org/download/mature.fa" "$miRBase_download_folder"
 # Define input and output file paths
 mature_file="$miRBase_download_folder/mature.fa"
 filtered_output="$miRBase_download_folder/miRBase_human.txt"
@@ -26,14 +26,14 @@ miRDB_download_folder="$project_data/miR_database/miRDB_database"
 miRTarBase_download_folder="$project_data/miR_database/miRTarBase_database"
 tarBase_download_folder="$project_data/miR_database/tarBase_database"
 # Download the files stored in TargetScan
-download_file.sh "https://www.targetscan.org/vert_80/vert_80_data_download/Conserved_Site_Context_Scores.txt.zip" "$targetScan_download_folder"
-download_file.sh "https://www.targetscan.org/vert_80/vert_80_data_download/Nonconserved_Site_Context_Scores.txt.zip" "$targetScan_download_folder"
+./download_file.sh "https://www.targetscan.org/vert_80/vert_80_data_download/Conserved_Site_Context_Scores.txt.zip" "$targetScan_download_folder"
+./download_file.sh "https://www.targetscan.org/vert_80/vert_80_data_download/Nonconserved_Site_Context_Scores.txt.zip" "$targetScan_download_folder"
 # Download the file stored in miRDB
-download_file.sh "https://mirdb.org/download/miRDB_v6.0_prediction_result.txt.gz" "$miRDB_download_folder"
+./download_file.sh "https://mirdb.org/download/miRDB_v6.0_prediction_result.txt.gz" "$miRDB_download_folder"
 # Download the file stored in miRTarBase
-download_file.sh "https://mirtarbase.cuhk.edu.cn/~miRTarBase/miRTarBase_2022/cache/download/9.0/miRTarBase_MTI.xlsx" "$miRTarBase_download_folder"
+./download_file.sh "https://mirtarbase.cuhk.edu.cn/~miRTarBase/miRTarBase_2022/cache/download/9.0/miRTarBase_MTI.xlsx" "$miRTarBase_download_folder"
 # Download the file stored in TarBase
-download_file.sh "https://dianalab.e-ce.uth.gr/tarbasev9/data/Homo_sapiens_TarBase-v9.tsv.gz" "$tarBase_download_folder"
+./download_file.sh "https://dianalab.e-ce.uth.gr/tarbasev9/data/Homo_sapiens_TarBase-v9.tsv.gz" "$tarBase_download_folder"
 
 # __ Chromosome files __
 # Base URL
@@ -47,11 +47,12 @@ for chr in {1..22} X Y M; do
   # Construct the full URL
   url="${base_url}/chr${chr}.fa.gz"
   # Check and download the file if it doesn't already exist
+  dest_file="$chr_download_folder/$chr${chr}.fa"
   if [ -f "$dest_file" ]; then
     echo "File chr${chr}.fa already exists, skipping download."
   else
     echo "Downloading chr${chr}...."
-    download_file.sh
+    ./download_file.sh "$url" "$chr_download_folder"
   fi
 done
 
