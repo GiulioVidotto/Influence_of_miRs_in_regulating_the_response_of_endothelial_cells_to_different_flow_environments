2defdfc753d465bec91680cf95e4929bd649fd47
Giulio Vidotto <134842955+GiulioVidotto@users.noreply.github.com>
2026-04-10 09:25:29 +0200

    Update and rename check_proteomics_for_IPA.Rmd to IPA_scripts/check_proteomics_for_IPA.R

--- IPA_scripts/check_proteomics_for_IPA.R
+++ IPA_scripts/check_proteomics_for_IPA.R
@@ -1,41 +1,13 @@
----
-title: "Untitled"
-author: "Graham Smith"
-date: "2026-02-11"
-output: html_document
----
+## read excel file and check 
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-## libs
-
-```{r getlibs}
-library(tidyverse)
-library(readxl)
-
-```
-
-## read Giulio's excel and check 
-
-Did both first file and second file. Second was obviuosly wrong 
-
-```{r pressure, echo=FALSE}
-
-# 
-dat = read_excel("~/data/Steve_White/results/Giulio/2026/IPA_input/excel_proteomics_first file.xlsx", sheet = 1 )
+dat = read_excel("~/excel_proteomics_first file.xlsx", sheet = 1 )
 
 for (col in grep("Abundance", names(dat), val = T)) {
   show(hist(log2(dat[[col]]), main = col, breaks = 50))
 }
 
-```
-
-Another sanity check is that (A/B) = (A/C)/(B/C)
-So AR(OSS vs ESS ) = AR(OSS vs LSS) / AR(ESS vs LSS)
-
-```{r check }
+# Another sanity check is that (A/B) = (A/C)/(B/C)
+# So AR(OSS vs ESS ) = AR(OSS vs LSS) / AR(ESS vs LSS)
 
 dat <- mutate(dat, Ab_OSS_vs_LSS_over_ESS_vs_LSS = protein_OSS_vs_LSS_Abundance_Ratio / protein_ESS_vs_LSS_Abundance_Ratio)
 
@@ -50,18 +22,11 @@ ggplot(dat2, aes(x = protein_OSS_vs_ESS_Abundance_Ratio, y = Ab_OSS_vs_LSS_over_
   geom_point(size = 0.1) + theme_bw(base_size = 14) + scale_x_log10() + scale_y_log10()
 
 
-```
+# output log trans version. 
 
+# Either remove (set to NA) values for FC and pval where FC is 100 or 0.01 (output file "proteomics_3contrasts_log2_table_rm100.txt") or leave them in ("proteomics_3contrasts_log2_table_with100.txt")
 
-output log trans version. 
-
-Either remove (set to NA) values for FC and pval where FC is 100 or 0.01 (output file "proteomics_3contrasts_log2_table_rm100.txt")
-or leave them in ("proteomics_3contrasts_log2_table_with100.txt")
-
-```{r pressure, echo=FALSE}
-
-# 
-dat = read_excel("~/data/Steve_White/results/Giulio/2026/IPA_input/excel_proteomics_first file.xlsx", sheet = 1 )
+dat = read_excel("~/excel_proteomics_first file.xlsx", sheet = 1 )
 
 rm_outliers = TRUE
 
@@ -108,15 +73,7 @@ for (col in grep("Abundance", names(dat), val = T)) {
 # how mmny NA
 colSums(is.na(dat))
 
-#write_tsv(dat, file =  file.path("~/data/Steve_White/results/Giulio/2026/IPA_input/", outfile))
-
-
-```
-
-
-Also ooutput the pval and expression just for the DE proteins (without the 100fold ones again) for DE proteins in OSS_vs_ESS
-
-```{r outnewres}
+# Also ooutput the pval and expression just for the DE proteins (without the 100fold ones again) for DE proteins in OSS_vs_ESS
 
 outdat <- select(dat, Protein_ID, Target_ID, protein_OSS_vs_ESS_Abundance_Log_Ratio,  protein_OSS_vs_ESS_adj_pvalue) %>%
   rename(protein_OSS_vs_ESS_Abundance_Log2_Ratio = protein_OSS_vs_ESS_Abundance_Log_Ratio) %>%
@@ -131,9 +88,4 @@ outdat <- left_join(outdat, symannot) %>%
   arrange(desc(protein_OSS_vs_ESS_Abundance_Log2_Ratio))
 
 write_csv(outdat, file = file.path(ipa_dir, "DE_proteins_OSS_vs_ESS.csv"))
-
-```
-
-
-
 
